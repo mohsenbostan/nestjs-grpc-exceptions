0d383c21708aac6a78b0e4085e7bec39446067a2
YoungKi Lyu <youngkiu@gmail.com>
2024-03-24 11:55:40 +0900

    Create grpc-exceptions-map.ts for GRPC_EXCEPTION_FROM_HTTP

--- lib/interceptors/http-to-grpc.interceptor.ts
+++ lib/interceptors/http-to-grpc.interceptor.ts
@@ -1,35 +1,12 @@
 import {
   CallHandler,
   ExecutionContext,
-  HttpStatus,
   Injectable,
   NestInterceptor,
 } from "@nestjs/common";
 import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
-import {
-  GrpcAbortedException,
-  GrpcAlreadyExistsException,
-  GrpcInternalException,
-  GrpcInvalidArgumentException,
-  GrpcNotFoundException,
-  GrpcPermissionDeniedException,
-  GrpcResourceExhaustedException,
-  GrpcUnauthenticatedException,
-  GrpcUnknownException,
-} from "../exceptions";
-
-const GRPC_EXCEPTION_FROM_HTTP: Record<number, any> = {
-  [HttpStatus.NOT_FOUND]: GrpcNotFoundException,
-  [HttpStatus.FORBIDDEN]: GrpcPermissionDeniedException,
-  [HttpStatus.METHOD_NOT_ALLOWED]: GrpcAbortedException,
-  [HttpStatus.INTERNAL_SERVER_ERROR]: GrpcInternalException,
-  [HttpStatus.TOO_MANY_REQUESTS]: GrpcResourceExhaustedException,
-  [HttpStatus.BAD_GATEWAY]: GrpcUnknownException,
-  [HttpStatus.CONFLICT]: GrpcAlreadyExistsException,
-  [HttpStatus.UNPROCESSABLE_ENTITY]: GrpcInvalidArgumentException,
-  [HttpStatus.UNAUTHORIZED]: GrpcUnauthenticatedException,
-};
+import { GRPC_EXCEPTION_FROM_HTTP } from "../utils";
 
 @Injectable()
 export class HttpToGrpcInterceptor implements NestInterceptor {
--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./error-object";
 export * from "./http-codes-map";
+export * from "./grpc-exceptions-map";
